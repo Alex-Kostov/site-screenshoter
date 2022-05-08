61333ba13e6b483c6cc6d427f32ed8a49dc776d8
Alex-Kostov <alex89773@gmail.com>
2022-05-09 00:07:31 +0300

    changing images filename and adding /images folder

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const Pageres = require('pageres');
 
-const urls = ['https://www.aax.media/about/', 'https://www.aax.media/', 'https://techsmith.com/'];
+const urls = ['https://www.youtube.com/', 'https://www.npmjs.com/package/pageres', 'https://wordpress.org/'];
 
 (async () => {
 	const pageres = await new Pageres({delay: 2});
 	for (const url of urls) {
-		await pageres.src( url, ['1920x1080']);
+		await pageres.src( url, ['1920x1080'], {filename: '<%= url %>-<%= date %>-<%= size %><%= crop %>'});
 	}
-	await pageres.dest(__dirname);
+	await pageres.dest(__dirname + '/images');
 	await pageres.run();
     console.log('Finished generating screenshots!');
 })();
